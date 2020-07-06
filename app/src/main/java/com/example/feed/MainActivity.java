1fa3e1ae2b1b0d44aa917aeafb7dc6576d5a3a58
Haswanth <55585654+Haswanthtummala@users.noreply.github.com>
2020-07-06 03:42:39 -0700

    first commit

---
+++ app/src/main/java/com/example/feed/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.feed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        android.R.attr feed;
+        setContentView(feed.layout.activity_main);
+    }
+}
